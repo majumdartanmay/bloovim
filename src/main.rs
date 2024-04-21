f1b2ab4cb8cd140b8f3572858abf2a12c6747e37
majumdartanmay <tanmay.majumdar@hotmail.com>
2024-04-21 23:07:26 +0530

    Found some template starting point. Need to refactor and break into smaller traits

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,100 @@
-use btleplug::api::{bleuuid::uuid_from_u16, Central, Manager as _, Peripheral as _, ScanFilter, WriteType};
-use btleplug::platform::{Adapter, Manager, Peripheral};
-use rand::{Rng, thread_rng};
+// Got this piece of code from 
+// https://github.com/deviceplug/btleplug/blob/master/examples/subscribe_notify_characteristic.rs
+
+use btleplug::api::{Central, CharPropFlags, Manager as _, Peripheral, ScanFilter};
+use btleplug::platform::Manager;
+use futures::stream::StreamExt;
 use std::error::Error;
-use std::thread;
 use std::time::Duration;
 use tokio::time;
 use uuid::Uuid;
 
-const LIGHT_CHARACTERISTIC_UUID: Uuid = uuid_from_u16(0xFFE9);
+/// Only devices whose name contains this string will be tried.
+const PERIPHERAL_NAME_MATCH_FILTER: &str = "OnePlus Bullets Wireless Z2";
+/// UUID of the characteristic for which we should subscribe to notifications.
+const NOTIFY_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x6e400002_b534_f393_67a9_e50e24dccA9e);
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let manager = Manager::new().await.unwrap();
 
-    // get the first bluetooth adapter
-    let adapters = manager.adapters().await?;
-    let central = adapters.into_iter().nth(0).unwrap();
-
-    // start scanning for devices
-    central.start_scan(ScanFilter::default()).await?;
-    // instead of waiting, you can use central.events() to get a stream which will
-    // notify you of new devices, for an example of that see examples/event_driven_discovery.rs
-    time::sleep(Duration::from_secs(2)).await;
-
-    // find the device we're interested in
-    let light = find_light(&central).await.unwrap();
-
-    // connect to the device
-    light.connect().await?;
-
-    // discover services and characteristics
-    light.discover_services().await?;
-
-    // find the characteristic we want
-    let chars = light.characteristics();
-    let cmd_char = chars.iter().find(|c| c.uuid == LIGHT_CHARACTERISTIC_UUID).unwrap();
-
-    // dance party
-    let mut rng = thread_rng();
-    for _ in 0..20 {
-        let color_cmd = vec![0x56, rng.gen(), rng.gen(), rng.gen(), 0x00, 0xF0, 0xAA];
-        light.write(&cmd_char, &color_cmd, WriteType::WithoutResponse).await?;
-        time::sleep(Duration::from_millis(200)).await;
+    let manager = Manager::new().await?;
+    let adapter_list = manager.adapters().await?;
+    if adapter_list.is_empty() {
+        eprintln!("No Bluetooth adapters found");
     }
-    Ok(())
-}
 
-async fn find_light(central: &Adapter) -> Option<Peripheral> {
-    for p in central.peripherals().await.unwrap() {
-        if p.properties()
+    for adapter in adapter_list.iter() {
+        println!("Starting scan...");
+        adapter
+            .start_scan(ScanFilter::default())
             .await
-            .unwrap()
-            .unwrap()
-            .local_name
-            .iter()
-            .any(|name| name.contains("LEDBlue"))
-        {
-            return Some(p);
+            .expect("Can't scan BLE adapter for connected devices...");
+        time::sleep(Duration::from_secs(2)).await;
+        let peripherals = adapter.peripherals().await?;
+
+        if peripherals.is_empty() {
+            eprintln!("->>> BLE peripheral devices were not found, sorry. Exiting...");
+        } else {
+            // All peripheral devices in range.
+            for peripheral in peripherals.iter() {
+                let properties = peripheral.properties().await?;
+                let is_connected = peripheral.is_connected().await?;
+
+                let local_name = properties
+                    .unwrap()
+                    .local_name
+                    .unwrap_or(String::from("(peripheral name unknown)"));
+                println!(
+                    "Peripheral {:?} is connected: {:?}",
+                    &local_name, is_connected
+                );
+                // Check if it's the peripheral we want.
+                if local_name.contains(PERIPHERAL_NAME_MATCH_FILTER) {
+                    println!("Found matching peripheral {:?}...", &local_name);
+                    if !is_connected {
+                        // Connect if we aren't already connected.
+                        if let Err(err) = peripheral.connect().await {
+                            eprintln!("Error connecting to peripheral, skipping: {}", err);
+                            continue;
+                        }
+                    }
+                    let is_connected = peripheral.is_connected().await?;
+                    println!(
+                        "Now connected ({:?}) to peripheral {:?}.",
+                        is_connected, &local_name
+                    );
+                    if is_connected {
+                        println!("Discover peripheral {:?} services...", local_name);
+                        peripheral.discover_services().await?;
+                        for characteristic in peripheral.characteristics() {
+                            println!("Checking characteristic {:?}", characteristic);
+                            // Subscribe to notifications from the characteristic with the selected
+                            // UUID.
+                            if characteristic.uuid == NOTIFY_CHARACTERISTIC_UUID
+                                && characteristic.properties.contains(CharPropFlags::NOTIFY)
+                            {
+                                println!("Subscribing to characteristic {:?}", characteristic.uuid);
+                                peripheral.subscribe(&characteristic).await?;
+                                // Print the first 4 notifications received.
+                                let mut notification_stream =
+                                    peripheral.notifications().await?.take(4);
+                                // Process while the BLE connection is not broken or stopped.
+                                while let Some(data) = notification_stream.next().await {
+                                    println!(
+                                        "Received data from {:?} [{:?}]: {:?}",
+                                        local_name, data.uuid, data.value
+                                    );
+                                }
+                            }
+                        }
+                        println!("Disconnecting from peripheral {:?}...", local_name);
+                        peripheral.disconnect().await?;
+                    }
+                } else {
+                    println!("Skipping unknown peripheral {:?}", peripheral);
+                }
+            }
         }
     }
-    None
+    Ok(())
 }
